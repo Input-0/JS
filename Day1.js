aa46ace46241f5e8785be999a03415185f8d07b8
input-0 <tyogi2015@gmail.com>
2026-03-13 10:38:12 +0530

    optional channing

---
+++ Day1.js
@@ -0,0 +1,34 @@
+// let ob = {
+//     name:"one",
+//     age:23,
+//     gender:"male",
+//     role: function(){
+//         let name = {
+//             postion:function(){
+//                 let log = "four"
+//                 let two = {
+//                     one : function(){
+//                         console.log(this.one)
+//                     }
+//                 }
+                
+//                 return log
+//             }
+//         }
+//         return name
+
+//     }
+// }
+// console.log(ob?.role()?.postion()?.log)
+
+
+
+
+let one ={
+    name: "two",
+    gender:"female"
+}
+
+let two = "name"
+
+console.log(one[two])
